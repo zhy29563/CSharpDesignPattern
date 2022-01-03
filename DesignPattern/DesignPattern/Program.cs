7b00779fcb68e98c671671e5588392e1cc3bc257
Steven Zhou <zhy29563@hotmail.com>
2022-01-03 18:44:08 +0800

    里氏代换原则案例分析

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -6,75 +6,120 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
-            // 定义输入法的一个实例，使用默认皮肤进行初始化
-            var souGouInput = new SouGouInput { Skin = new DefaultSkin() };
-            // 显示当前使用的皮肤
-            souGouInput.Display();
+            var graphicSystem = new GraphicSystem();
+            var rectangle = new Rectangle();
+            rectangle.SetLength(20);
+            rectangle.SetWidth(10);
+            graphicSystem.Rectangle = rectangle;
             
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("开始切换皮肤");
-            // 切换为自定义皮肤
-            souGouInput.Skin = new CustomSkin();
-            // 显示当前使用的皮肤
-            souGouInput.Display();
+            graphicSystem.Resize();
+            graphicSystem.PrintLengthAndWidth();
+
+            Console.WriteLine("============");
+
+            var square = new Square();
+            square.SetLength(10);
+            graphicSystem.Rectangle = square;
+            graphicSystem.Resize();
+            graphicSystem.PrintLengthAndWidth();
+        }
+    }
+
+    /// <summary>
+    /// 图形系统
+    /// </summary>
+    public class GraphicSystem
+    {
+        /// <summary>
+        /// 基类对象属性
+        /// </summary>
+        public Rectangle Rectangle { get; set; }
+        
+        /// <summary>
+        /// 当宽度小于等于长度时，直到宽度大于长度
+        /// </summary>
+        public void Resize()
+        {
+            var index = 0;
+            while (this.Rectangle.GetWidth() <= this.Rectangle.GetLength())
+            {
+                Console.WriteLine($"正在执行第 {index++} 次循环");
+                this.Rectangle.SetWidth(this.Rectangle.GetWidth() + 1);
+            }
+        }
+
+        /// <summary>
+        /// 打印当前的长度和宽度
+        /// </summary>
+        public void PrintLengthAndWidth()
+        {
+            Console.WriteLine(this.Rectangle.GetLength());
+            Console.WriteLine(this.Rectangle.GetWidth());
         }
     }
     
     /// <summary>
-    /// 输入法皮肤的抽象基类
+    /// 矩形类
     /// </summary>
-    public abstract class AbstractSkin
+    public class Rectangle
     {
         /// <summary>
-        /// 用于显示当前所使用的皮肤
+        /// 矩形长度
         /// </summary>
-        public abstract void Display();
+        private double length;
+        /// <summary>
+        /// 矩形宽度
+        /// </summary>
+        private double width;
+
+        /// <summary>
+        /// 获取长度
+        /// </summary>
+        /// <returns>长度</returns>
+        public double GetLength() => this.length;
+
+        /// <summary>
+        /// 设置长度
+        /// </summary>
+        /// <param name="length">长度</param>
+        public virtual void SetLength(double length) => this.length = length;
+
+        /// <summary>
+        /// 获取宽度
+        /// </summary>
+        /// <returns>宽度</returns>
+        public double GetWidth() => this.width;
+
+        /// <summary>
+        /// 设置宽度
+        /// </summary>
+        /// <param name="width">宽度</param>
+        public virtual void SetWidth(double width)=>this.width = width;
     }
 
     /// <summary>
-    /// 默认皮肤
+    /// 正方形类
     /// </summary>
-    public class DefaultSkin : AbstractSkin
+    public class Square : Rectangle
     {
         /// <summary>
-        /// <inheritdoc cref="AbstractSkin"/>
+        /// <inheritdoc cref="Rectangle"/>
         /// </summary>
-        public override void Display()
+        /// <param name="width">宽度</param>
+        public override void SetWidth(double width)
         {
-            Console.WriteLine(nameof(DefaultSkin));
+            base.SetLength(width);
+            base.SetWidth(width);
         }
-    }
 
-    /// <summary>
-    /// 自定义皮肤
-    /// </summary>
-    public class CustomSkin : AbstractSkin
-    {
         /// <summary>
-        /// <inheritdoc cref="AbstractSkin"/>
+        /// <inheritdoc cref="Rectangle"/>
         /// </summary>
-        public override void Display()
+        /// <param name="length">长度</param>
+        public override void SetLength(double length)
         {
-            Console.WriteLine(nameof(CustomSkin));
-        }
-    }
-
-    /// <summary>
-    /// 搜狗输入法类
-    /// </summary>
-    public class SouGouInput
-    {
-        /// <summary>
-        /// 皮肤抽象积累的成员变量
-        /// </summary>
-        public AbstractSkin Skin {get;set;}
-
-        /// <summary>
-        /// 用于显示当前所使用的皮肤
-        /// </summary>
-        public void Display()
-        {
-            Skin?.Display();
+            base.SetLength(length);
+            base.SetWidth(length);
         }
     }
 }
