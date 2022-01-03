e117a185675c00da4b35a3de177f398475e3e5df
Steven Zhou <zhy29563@hotmail.com>
2022-01-03 18:46:04 +0800

    里氏代换之问题修复

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -10,7 +10,7 @@ namespace DesignPattern
             var rectangle = new Rectangle();
             rectangle.SetLength(20);
             rectangle.SetWidth(10);
-            graphicSystem.Rectangle = rectangle;
+            graphicSystem.Quadrilateral = rectangle;
             
             graphicSystem.Resize();
             graphicSystem.PrintLengthAndWidth();
@@ -18,8 +18,8 @@ namespace DesignPattern
             Console.WriteLine("============");
 
             var square = new Square();
-            square.SetLength(10);
-            graphicSystem.Rectangle = square;
+            square.SetSide(10);
+            graphicSystem.Quadrilateral = square;
             graphicSystem.Resize();
             graphicSystem.PrintLengthAndWidth();
         }
@@ -33,18 +33,21 @@ namespace DesignPattern
         /// <summary>
         /// 基类对象属性
         /// </summary>
-        public Rectangle Rectangle { get; set; }
+        public Quadrilateral Quadrilateral { get; set; }
         
         /// <summary>
         /// 当宽度小于等于长度时，直到宽度大于长度
         /// </summary>
         public void Resize()
         {
+            if (!(this.Quadrilateral is Rectangle rectangle))
+                return;
+            
             var index = 0;
-            while (this.Rectangle.GetWidth() <= this.Rectangle.GetLength())
+            while (this.Quadrilateral.GetWidth() <= this.Quadrilateral.GetLength())
             {
                 Console.WriteLine($"正在执行第 {index++} 次循环");
-                this.Rectangle.SetWidth(this.Rectangle.GetWidth() + 1);
+                rectangle.SetWidth(this.Quadrilateral.GetWidth() + 1);
             }
         }
 
@@ -53,27 +56,46 @@ namespace DesignPattern
         /// </summary>
         public void PrintLengthAndWidth()
         {
-            Console.WriteLine(this.Rectangle.GetLength());
-            Console.WriteLine(this.Rectangle.GetWidth());
+            Console.WriteLine(this.Quadrilateral.GetLength());
+            Console.WriteLine(this.Quadrilateral.GetWidth());
         }
+    }
+    
+    /// <summary>
+    /// 四边形基类
+    /// </summary>
+    public interface Quadrilateral 
+    {
+        /// <summary>
+        /// 获取长度
+        /// </summary>
+        /// <returns>长度</returns>
+        double GetLength();
+        
+        /// <summary>
+        /// 获取宽度
+        /// </summary>
+        /// <returns>宽度</returns>
+        double GetWidth();
     }
     
     /// <summary>
     /// 矩形类
     /// </summary>
-    public class Rectangle
+    public class Rectangle : Quadrilateral
     {
         /// <summary>
         /// 矩形长度
         /// </summary>
         private double length;
+        
         /// <summary>
         /// 矩形宽度
         /// </summary>
         private double width;
 
         /// <summary>
-        /// 获取长度
+        /// <inheritdoc cref="Quadrilateral"/>
         /// </summary>
         /// <returns>长度</returns>
         public double GetLength() => this.length;
@@ -82,10 +104,10 @@ namespace DesignPattern
         /// 设置长度
         /// </summary>
         /// <param name="length">长度</param>
-        public virtual void SetLength(double length) => this.length = length;
+        public void SetLength(double length) => this.length = length;
 
         /// <summary>
-        /// 获取宽度
+        /// <inheritdoc cref="Quadrilateral"/>
         /// </summary>
         /// <returns>宽度</returns>
         public double GetWidth() => this.width;
@@ -94,32 +116,35 @@ namespace DesignPattern
         /// 设置宽度
         /// </summary>
         /// <param name="width">宽度</param>
-        public virtual void SetWidth(double width)=>this.width = width;
+        public void SetWidth(double width)=>this.width = width;
     }
 
     /// <summary>
     /// 正方形类
     /// </summary>
-    public class Square : Rectangle
+    public class Square : Quadrilateral
     {
         /// <summary>
-        /// <inheritdoc cref="Rectangle"/>
+        /// 正方向边长
         /// </summary>
-        /// <param name="width">宽度</param>
-        public override void SetWidth(double width)
-        {
-            base.SetLength(width);
-            base.SetWidth(width);
-        }
-
+        private double side; 
+        
         /// <summary>
-        /// <inheritdoc cref="Rectangle"/>
+        /// 设置正方向边长
         /// </summary>
-        /// <param name="length">长度</param>
-        public override void SetLength(double length)
-        {
-            base.SetLength(length);
-            base.SetWidth(length);
-        }
+        /// <param name="side">边长</param>
+        public void SetSide(double side)=>this.side = side; 
+        
+        /// <summary>
+        /// <inheritdoc cref="Quadrilateral"/>
+        /// </summary>
+        /// <returns>长度</returns>
+        public double GetLength() => this.side; 
+        
+        /// <summary>
+        /// <inheritdoc cref="Quadrilateral"/>
+        /// </summary>
+        /// <returns>宽度</returns>
+        public double GetWidth() => this.side;
     }
 }
