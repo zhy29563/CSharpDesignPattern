094c50bde19c325c98e5e49d13f5851c2bacad60
Steven Zhou <zhy29563@hotmail.com>
2022-01-04 21:52:47 +0800

    合成复用原则之问题解决

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -6,18 +6,48 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
-            new RedPetrolCar().Move();
-            new RedElectricCar().Move();
-            new WhitePetrolCar().Move();
-            new WhiteElectricCar().Move();
+            new PetrolCar() {Color = new WhiteColor()}.Move();
+            new ElectricCar() {Color = new RedColor()}.Move();
         }
     }
     
+    /// <summary>
+    /// 颜色接口
+    /// </summary>
+    public interface IColor
+    {
+        string Name {get;}
+    }
+
+    /// <summary>
+    /// 红色
+    /// </summary>
+    public class RedColor : IColor
+    {
+        public string Name => "Red";
+    }
+
+    /// <summary>
+    /// 白色
+    /// </summary>
+    public class WhiteColor : IColor
+    {
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        public string Name => "Red";
+    }
+
     /// <summary>
     /// 抽象汽车类
     /// </summary>
     public abstract class Car
     {
+        /// <summary>
+        /// 聚合颜色属性
+        /// </summary>
+        public IColor Color {get;set;}
+        
         /// <summary>
         /// 行驶功能
         /// </summary>
@@ -27,58 +57,22 @@ namespace DesignPattern
     /// <summary>
     /// 汽油车类
     /// </summary>
-    public abstract class PetrolCar : Car
-    {
-    }
-
-    /// <summary>
-    /// 电动汽车类
-    /// </summary>
-    public abstract class ElectricCar : Car
-    {
-    }
-
-    /// <summary>
-    /// 红色汽油汽车类
-    /// </summary>
-    public class RedPetrolCar : PetrolCar
+    public class PetrolCar : Car
     {
         /// <summary>
         /// <inheritdoc cref="Move"/>
         /// </summary>
-        public override void Move() => Console.WriteLine(nameof(RedPetrolCar));
+        public override void Move() => Console.WriteLine($"{this.Color.Name} {nameof(PetrolCar)} is " + nameof(Move));
     }
 
     /// <summary>
-    /// 白色汽油汽车类
+    /// 电动车类
     /// </summary>
-    public class WhitePetrolCar : PetrolCar
+    public class ElectricCar : Car
     {
         /// <summary>
         /// <inheritdoc cref="Move"/>
         /// </summary>
-        public override void Move() => Console.WriteLine(nameof(WhitePetrolCar));
-    }
-
-    /// <summary>
-    /// 红色电动汽车类
-    /// </summary>
-    public class RedElectricCar : ElectricCar
-    {
-        /// <summary>
-        /// <inheritdoc cref="Move"/>
-        /// </summary>
-        public override void Move() => Console.WriteLine(nameof(RedPetrolCar));
-    }
-
-    /// <summary>
-    /// 白色电动汽车类
-    /// </summary>
-    public class WhiteElectricCar : ElectricCar
-    {
-        /// <summary>
-        /// <inheritdoc cref="Move"/>
-        /// </summary>
-        public override void Move() => Console.WriteLine(nameof(WhitePetrolCar));
+        public override void Move() => Console.WriteLine($"{this.Color.Name}  {nameof(ElectricCar)} is " + nameof(Move));
     }
 }
