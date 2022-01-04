1e478f38e18a307b0c850ebd21343617f78199fe
Steven Zhou <zhy29563@hotmail.com>
2022-01-04 20:22:52 +0800

    接口隔离原则之问题引入

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -6,130 +6,52 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
-            var c = new Computer()
-            {
-                Cpu = new IntelCpu(),
-                Memory = new KingstonMemory(),
-                HardDisk = new XiJieHardDisk()
-            };
-
-            c.Run();
-        }
-    }
-    
-    public class Computer
-    {
-        /// <summary>
-        /// CPU属性
-        /// </summary>
-        public ICpu Cpu { get; set; }
-        
-        /// <summary>
-        /// 内存属性
-        /// </summary>
-        public IMemory Memory { get; set; }
-        
-        /// <summary>
-        /// 硬盘属性
-        /// </summary>
-        public IHardDisk HardDisk { get; set; }
-
-        /// <summary>
-        /// 运行
-        /// </summary>
-        public void Run()
-        {
-            Console.WriteLine("运行计算机");
-            var data = this.HardDisk.Get();
-            Console.WriteLine("从硬盘上获取的数据是：" + data);
-            this.Cpu.Run();
-            this.Memory.Save();
+            var door = new ASafetyDoor();
+            door.AntiTheft();
+            door.FireProof();
+            door.WaterProof();
         }
     }
     
     /// <summary>
-    /// 硬盘接口
+    /// 防盗门接口
     /// </summary>
-    public interface IHardDisk
+    public interface ISafetyDoor
     {
         /// <summary>
-        /// 保存数据
+        /// 防盗功能
         /// </summary>
-        /// <param name="data">数据</param>
-        void Save(string data);
+        void AntiTheft();
         
         /// <summary>
-        /// 获取数据
+        /// 防火功能
         /// </summary>
-        /// <returns>数据</returns>
-        string Get();
+        void FireProof();
+        
+        /// <summary>
+        /// 防水功能
+        /// </summary>
+        void WaterProof();
     }
 
     /// <summary>
-    /// 内存接口
+    /// A 品牌功能
     /// </summary>
-    public interface IMemory
+    public class ASafetyDoor : ISafetyDoor
     {
         /// <summary>
-        /// 保存
+        /// <inheritdoc cref="ISafetyDoor"/>
         /// </summary>
-        void Save();
-    }
-
-    /// <summary>
-    /// CPU接口
-    /// </summary>
-    public interface ICpu
-    {
-        /// <summary>
-        /// 运行
-        /// </summary>
-        void Run();
-    }
-
-    /// <summary>
-    /// 希捷硬盘类
-    /// </summary>
-    public class XiJieHardDisk : IHardDisk
-    {
-        /// <summary>
-        /// <inheritdoc cref="IHardDisk"/>
-        /// </summary>
-        /// <param name="data">数据</param>
-        public void Save(string data) => Console.WriteLine("使用希捷硬盘存储数据为：" + data);
+        public void AntiTheft() => Console.WriteLine("防盗");
 
         /// <summary>
-        /// <inheritdoc cref="IHardDisk"/>
+        /// <inheritdoc cref="ISafetyDoor"/>
         /// </summary>
-        /// <returns>数据</returns>
-        public string Get()
-        {
-            Console.WriteLine("使用希捷希捷硬盘取数据");
-            return "数据";
-        }
-    }
+        public void FireProof() => Console.WriteLine("防火");
 
-    /// <summary>
-    /// 金士顿内存
-    /// </summary>
-    public class KingstonMemory : IMemory
-    {
         /// <summary>
-        /// 保存
+        /// <inheritdoc cref="ISafetyDoor"/>
         /// </summary>
-        public void Save() => Console.WriteLine("使用金士顿内存条");
+        public void WaterProof() => Console.WriteLine("防水");
     }
-
-    /// <summary>
-    /// CPU
-    /// </summary>
-    public class IntelCpu : ICpu
-    {
-        /// <summary>
-        /// 运行
-        /// </summary>
-        public void Run() => Console.WriteLine("使用Intel处理器");
-    }
-
-    
 }
