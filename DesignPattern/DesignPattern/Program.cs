42cfc7c2c9621077a5776185a40bca36dad63b7f
Steven Zhou <zhy29563@hotmail.com>
2022-01-04 20:47:43 +0800

    接口隔离原则之问题解决

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -6,52 +6,86 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
-            var door = new ASafetyDoor();
-            door.AntiTheft();
-            door.FireProof();
-            door.WaterProof();
+            var doorA = new ASafetyDoor();
+            doorA.AntiTheft();
+            doorA.Fireproof();
+            doorA.Waterproof();
+
+            Console.WriteLine("============");
+            var doorB = new BSafetyDoor();
+            doorB.AntiTheft();
+            doorB.Fireproof();
         }
     }
     
     /// <summary>
-    /// 防盗门接口
+    /// 防盗接口
     /// </summary>
-    public interface ISafetyDoor
+    public interface IAntiTheft
     {
         /// <summary>
         /// 防盗功能
         /// </summary>
         void AntiTheft();
-        
-        /// <summary>
-        /// 防火功能
-        /// </summary>
-        void FireProof();
-        
-        /// <summary>
-        /// 防水功能
-        /// </summary>
-        void WaterProof();
     }
 
     /// <summary>
-    /// A 品牌功能
+    /// 防水接口
     /// </summary>
-    public class ASafetyDoor : ISafetyDoor
+    public interface IWaterproof
     {
         /// <summary>
-        /// <inheritdoc cref="ISafetyDoor"/>
+        /// 防水功能
+        /// </summary>
+        void Waterproof();
+    }
+
+    /// <summary>
+    /// 防火接口
+    /// </summary>
+    public interface IFireproof
+    {
+        /// <summary>
+        /// 防火功能
+        /// </summary>
+        void Fireproof();
+    }
+
+
+    /// <summary>
+    /// A 品牌安全门
+    /// </summary>
+    public class ASafetyDoor :  IAntiTheft, IFireproof, IWaterproof
+    {
+        /// <summary>
+        /// <inheritdoc cref="AntiTheft"/>
         /// </summary>
         public void AntiTheft() => Console.WriteLine("防盗");
 
         /// <summary>
-        /// <inheritdoc cref="ISafetyDoor"/>
+        /// <inheritdoc cref="Fireproof"/>
         /// </summary>
-        public void FireProof() => Console.WriteLine("防火");
+        public void Fireproof() => Console.WriteLine("防火");
 
         /// <summary>
-        /// <inheritdoc cref="ISafetyDoor"/>
+        /// <inheritdoc cref="Waterproof"/>
         /// </summary>
-        public void WaterProof() => Console.WriteLine("防水");
+        public void Waterproof() => Console.WriteLine("防水");
+    }
+
+    /// <summary>
+    /// B 品牌安全门
+    /// </summary>
+    public class BSafetyDoor : IAntiTheft, IFireproof
+    {
+        /// <summary>
+        /// <inheritdoc cref="AntiTheft"/>
+        /// </summary>
+        public void AntiTheft() => Console.WriteLine("防盗");
+
+        /// <summary>
+        /// <inheritdoc cref="Fireproof"/>
+        /// </summary>
+        public void Fireproof() => Console.WriteLine("防火");
     }
 }
