c307b6d68c2b9300ee9e18319b4068bab8043ca6
Steven Zhou <zhy29563@hotmail.com>
2022-01-07 21:00:54 +0800

    工厂模式引入

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -6,19 +6,85 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
-            var instance1 = Singleton.Instance;
-            var instance2 = Singleton.Instance;
-            Console.WriteLine(instance1 == instance2);
+            var store = new CoffeeStore();
+            var coffee = store.OrderCoffee("american");
+            Console.WriteLine(coffee.Name);
+	
+            Console.WriteLine("----------------------------");
+            coffee = store.OrderCoffee("latte");
+            Console.WriteLine(coffee.Name);
         }
     }
     
     /// <summary>
-    /// 懒汉式：延迟初始化方式
+    /// 咖啡的抽象基类
     /// </summary>
-    public sealed class Singleton
+    public abstract class Coffee
     {
-        private static readonly Lazy<Singleton> Lazy = new Lazy<Singleton>(()=> new Singleton());
-        public static Singleton Instance => Lazy.Value;
-        private Singleton() { }
+        /// <summary>
+        /// 咖啡名称
+        /// </summary>
+        public abstract string Name {get; }
+
+        /// <summary>
+        /// 加糖功能
+        /// </summary>
+        public void AddSugar() => Console.WriteLine("加糖");
+
+        /// <summary>
+        /// 加奶功能
+        /// </summary>
+        public void AddMilk() => Console.WriteLine("加奶");
+    }
+
+    public class LatteCoffee : Coffee
+    {
+        /// <summary>
+        /// <inheritdoc cref="Name"/>
+        /// </summary>
+        public override string Name => "拿铁咖啡";
+    }
+
+    public class AmericanCoffee : Coffee
+    {
+        /// <summary>
+        /// <inheritdoc cref="Name"/>
+        /// </summary>
+        public override string Name => "美式咖啡";
+    }
+
+    /// <summary>
+    /// 咖啡店点餐系统，该系统可以为顾客提供美式咖啡与拿铁咖啡
+    /// </summary>
+    public class CoffeeStore
+    {
+        /// <summary>
+        /// 点咖啡功能
+        /// </summary>
+        /// <param name="type">咖啡名</param>
+        /// <returns>咖啡<see cref="Coffee"/></returns>
+        /// <exception cref="Exception"></exception>
+        public Coffee OrderCoffee(string type)
+        {
+            Coffee coffee;
+            switch (type)
+            {
+                case "american":
+                    coffee = new AmericanCoffee();
+                    break;
+                case "latte":
+                    coffee = new LatteCoffee();
+                    break;
+                default:
+                    throw new Exception("对不起，您所点的咖啡没有");
+            }
+
+            // 加奶
+            coffee.AddMilk();
+            // 加糖
+            coffee.AddSugar();
+
+            return coffee;
+        }
     }
 }
