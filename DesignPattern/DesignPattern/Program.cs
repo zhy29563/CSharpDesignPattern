e736964e964a76ca8de628dfffbf4fe00d716fcb
Steven Zhou <zhy29563@hotmail.com>
2022-01-07 20:08:58 +0800

    单例模式，懒汉式：单线程安全

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -6,73 +6,20 @@ namespace DesignPattern
     {
         public static void Main(string[] args)
         {
-            new PetrolCar() {Color = new WhiteColor()}.Move();
-            new ElectricCar() {Color = new RedColor()}.Move();
+            var instance1 = Singleton.Instance;
+            var instance2 = Singleton.Instance;
+            Console.WriteLine(instance1 == instance2);
         }
     }
     
     /// <summary>
-    /// 颜色接口
+    /// 懒汉式：单线程安全
     /// </summary>
-    public interface IColor
+    public sealed class Singleton
     {
-        string Name {get;}
-    }
+        private static Singleton _instance;
+        public static Singleton Instance => _instance ?? (_instance = new Singleton());
 
-    /// <summary>
-    /// 红色
-    /// </summary>
-    public class RedColor : IColor
-    {
-        public string Name => "Red";
-    }
-
-    /// <summary>
-    /// 白色
-    /// </summary>
-    public class WhiteColor : IColor
-    {
-        /// <summary>
-        /// 颜色
-        /// </summary>
-        public string Name => "Red";
-    }
-
-    /// <summary>
-    /// 抽象汽车类
-    /// </summary>
-    public abstract class Car
-    {
-        /// <summary>
-        /// 聚合颜色属性
-        /// </summary>
-        public IColor Color {get;set;}
-        
-        /// <summary>
-        /// 行驶功能
-        /// </summary>
-        public abstract void Move();
-    }
-
-    /// <summary>
-    /// 汽油车类
-    /// </summary>
-    public class PetrolCar : Car
-    {
-        /// <summary>
-        /// <inheritdoc cref="Move"/>
-        /// </summary>
-        public override void Move() => Console.WriteLine($"{this.Color.Name} {nameof(PetrolCar)} is " + nameof(Move));
-    }
-
-    /// <summary>
-    /// 电动车类
-    /// </summary>
-    public class ElectricCar : Car
-    {
-        /// <summary>
-        /// <inheritdoc cref="Move"/>
-        /// </summary>
-        public override void Move() => Console.WriteLine($"{this.Color.Name}  {nameof(ElectricCar)} is " + nameof(Move));
+        private Singleton() { }
     }
 }
